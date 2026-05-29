40c72275fcd81f5be9aafe36a1de3f8d47863c41
tenfilm13-eng <tenfilm13@gmail.com>
2026-05-30 00:53:36 +0200

    Update data.js

--- data.js
+++ data.js
@@ -91,7 +91,7 @@ const CATALOG = [
     generos: ["Drama", "Thriller"],
     avaliacao: 7.1,
     poster: "https://br.web.img3.acsta.net/pictures/18/10/18/07/54/1893178.jpg",
-    duracao: "2 temporadas · 8 episódios cada",
+    duracao: "2 temporadas · 8 episódios",
     qualidade: "1080p",
     trailer: "https://youtu.be/Oqv81BdRs7w?si=nKoXtGhtRT5JYZtw",
     pais: "Espanha",
@@ -306,9 +306,365 @@ const CATALOG = [
         }))
       }
     ]
-  }
+  },
 
+  {
+    id: 7,
+    tipo: "filme",
+    titulo: "Velocidade Furiosa 5",
+    ano: 2011,
+    sinopse: "Dominic Toretto é resgatado da prisão por Brian e Mia. Foragidos no Rio de Janeiro, eles planeiam um assalto a um comboio de carros de luxo. Após descobrirem que os veículos escondem segredos de um barão do crime local, decidem roubar a sua fortuna milionária",
+    generos: ["Ação", "Crime"],
+    avaliacao: 7.3,
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs4gKTqEHSNf1RtOz2UAkzAyB6IetrK-kIvqXOISW6fO6fPIX-AUR5G_EgHTUWJLx3evewlx_oQDhbLXrHC1FLU1M6UbDVZo8oKBQ7tg&s=10",
+    videoUrl: "",
+    idiomas: ["Português", "Inglês"],
+    qualidade: "1080p",
+    trailer: "https://youtu.be/E1owNxw0cz0?si=D-khhBxu3LeXzSg7",
+    duracao: "2h 10min",
+    pais: "Brasil"
+  },
 
+  {
+    id: 8,
+    tipo: "filme",
+    titulo: " Os Pinguins de Madagáscar 5",
+    ano: 2014,
+    sinopse: "são um famoso grupo de espionagem formado por quatro aves — Capitão, Kowalski, Rico e Recruta",
+    generos: ["Infantil/", "Aventura "],
+    avaliacao: 6.7,
+    poster: "https://play-lh.googleusercontent.com/vwhjRgQiVtokPOiYM11XCOVHSRuUBeTtir35DTshjPqrTFUzXEZazao01f-0EtkrZDw",
+    videoUrl: "",
+    idiomas: ["Português", "Inglês"],
+    qualidade: "1080p",
+    trailer: "https://youtu.be/i6ZYyAa03p0?si=N_jq6voNUXIEuBCJ",
+    duracao: "1h 32min",
+    pais: "EUA"
+  },
+
+  {
+    id: 9,
+    tipo: "filme",
+    titulo: "Madagascar",
+    ano: 2005,
+    sinopse: "acompanha quatro animais mimados do zoológico de Nova York — Alex (leão), Marty (zebra), Melman (girafa) e Glória (hipopótamo). Após uma tentativa de fuga, eles são capturados e enviados para uma reserva na África. O navio sofre um acidente, e eles acabam naufragando na ilha de Madagascar",
+    generos: ["Infantil/", "Comédia "],
+    avaliacao: 6.9,
+    poster: "https://br.web.img2.acsta.net/medias/nmedia/18/91/54/02/20150796.jpg",
+    videoUrl: "",
+    idiomas: ["Português", "Inglês"],
+    qualidade: "1080p",
+    trailer: "https://youtu.be/orAqhC-Hp_o?si=9d2MPtR9l59YdBLW",
+    duracao: "1h 26min",
+    pais: "EUA"
+  },
+
+   {
+    id: 10,
+    tipo: "filme",
+    titulo: "Zona de Perigo",
+    ano: 2021,
+    sinopse: "Tom Hardy (Bruce Willis) é um policial de Chicago de uma família com forte tradição na corporação. Após denunciar métodos violentos do seu parceiro, este último morre e Hardy é transferido para a equipe de resgate fluvial.",
+    generos: ["Ação/", "Ficção científica "],
+    avaliacao: 6.6,
+    poster: "https://play-lh.googleusercontent.com/proxy/PGwbRgfJFGEj3vmV95caKzmoET-ZZBDeF963hgMOEtjqjnco-hOzE1rpsz0DTtKVvK6XAE9VpKqVE2f5jwJ3iuzgt3fPIvy3qU1QX0dwZH17kiv2Hs4Rg_sSAfcpjaGFEhj1nNy80I4Lz1d8ExEAMZRHIjW93vrL8X3Ejg=w240-h480-rw",
+    videoUrl: "",
+    idiomas: ["Português", "Inglês"],
+    qualidade: "1080p",
+    trailer: "https://youtu.be/4cOLIdNUdIU?si=BvisgVDmbHpHnm3r",
+    duracao: "1h 55min",
+    pais: "EUA"
+  },
+
+  {
+    id: 11,
+    tipo: "serie",
+    titulo: "La Casa de Papel",
+    ano: 2017,
+    sinopse: "Acompanha um grupo de criminosos liderados por um gênio conhecido como O Professor. Eles planejam e executam assaltos meticulosos à Fábrica Nacional de Moeda e Timbre e ao Banco Central da Espanha, focando em imprimir o próprio dinheiro e desestabilizar o sistema financeiro com um toque de rebeldia",
+    generos: ["Thriller"],
+    qualidade: "1080p",
+    trailer: "https://youtu.be/jkzl8ZAppqE?si=fMdPri9VhVUPfE45",
+    avaliacao: 8.3,
+    poster: "https://static.wikia.nocookie.net/dublagem/images/c/ca/La_Casa_de_Papel.jpg/revision/latest?cb=20210906003054&path-prefix=pt-br",
+    duracao: "5 temporadas",
+    pais: "EUA",
+    temporadas: [
+      {
+        numero: 1,
+        titulo: "Temporada 1",
+        episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+         
+        ]
+      },
+      {
+        numero: 2,
+        titulo: "Temporada 2",
+        episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+         
+        ]
+      },
+      {
+        numero: 3,
+        titulo: "Temporada 3",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+         
+         
+        ]
+      },
+      {
+        numero: 4,
+        titulo: "Temporada 4",
+        episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+        
+         
+        ]
+      },
+      {
+        numero: 5,
+        titulo: "Temporada 5",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+         
+        ]
+      }
+    ]
+  },
+
+  {
+    id: 12,
+    tipo: "serie",
+    titulo: "Fear the Walking Dead.",
+    ano: 2015,
+    sinopse: "Focado em mostrar o colapso da sociedade e o início do apocalipse zumbi sob a perspectiva de uma família disfuncional em Los Angeles",
+    generos: ["Terror"],
+    qualidade: "1080p",
+    trailer: "https://youtu.be/yzXglr5bc3w?si=26Y8qH9w2VvvM-Z3",
+    avaliacao: 6.8,
+    poster: "https://br.web.img2.acsta.net/c_310_420/pictures/19/10/15/00/15/4502728.jpg",
+    duracao: "8 temporadas",
+    pais: "EUA",
+    temporadas: [
+      {
+        numero: 1,
+        titulo: "Temporada 1",
+        episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+         
+        ]
+      },
+      {
+        numero: 2,
+        titulo: "Temporada 2",
+        episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+          { titulo: "Episódio 14", videoUrl: "" },
+          { titulo: "Episódio 15", videoUrl: "" },
+          
+        ]
+      },
+      {
+        numero: 3,
+        titulo: "Temporada 3",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+          { titulo: "Episódio 14", videoUrl: "" },
+          { titulo: "Episódio 15", videoUrl: "" },
+          { titulo: "Episódio 16", videoUrl: "" },
+          
+         
+         
+        ]
+      },
+      {
+        numero: 4,
+        titulo: "Temporada 4",
+        episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" }, 
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+          { titulo: "Episódio 14", videoUrl: "" },
+          { titulo: "Episódio 15", videoUrl: "" },
+          { titulo: "Episódio 16", videoUrl: "" },
+        
+         
+        ]
+      },
+      {
+        numero: 5,
+        titulo: "Temporada 5",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+          { titulo: "Episódio 14", videoUrl: "" },
+          { titulo: "Episódio 15", videoUrl: "" },
+          { titulo: "Episódio 16", videoUrl: "" },
+         
+        ]
+      },
+      {
+        numero: 6,
+        titulo: "Temporada 6",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+          { titulo: "Episódio 14", videoUrl: "" },
+          { titulo: "Episódio 15", videoUrl: "" },
+          { titulo: "Episódio 16", videoUrl: "" },
+         
+        ]
+      },
+      {
+        numero: 7,
+        titulo: "Temporada 7",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+          { titulo: "Episódio 13", videoUrl: "" },
+          { titulo: "Episódio 14", videoUrl: "" },
+          { titulo: "Episódio 15", videoUrl: "" },
+          { titulo: "Episódio 16", videoUrl: "" },
+         
+        ]
+      },
+      {
+        numero: 8,
+        titulo: "Temporada 8",
+       episodios: [
+          { titulo: "Episódio 1", videoUrl: "" },
+          { titulo: "Episódio 2", videoUrl: "" },
+          { titulo: "Episódio 3", videoUrl: "" },
+          { titulo: "Episódio 4", videoUrl: "" },
+          { titulo: "Episódio 5", videoUrl: "" },
+          { titulo: "Episódio 6", videoUrl: "" },
+          { titulo: "Episódio 7", videoUrl: "" },
+          { titulo: "Episódio 8", videoUrl: "" },
+          { titulo: "Episódio 9", videoUrl: "" },
+          { titulo: "Episódio 10", videoUrl: "" },
+          { titulo: "Episódio 11", videoUrl: "" },
+          { titulo: "Episódio 12", videoUrl: "" },
+        ]
+      }
+    ]
+  },
   /* ─────────────────────────────────────────────────────────
      PARA ADICIONAR NOVO FILME:
      ,{
